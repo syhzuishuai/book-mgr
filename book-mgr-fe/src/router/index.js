866c06ca595a6ae8e404ea86d1947126265916d2
shenyunhe <7520083+hehewudi@user.noreply.gitee.com>
2022-12-31 14:30:36 +0800

    完成前端登陆注册相关的界面

--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: '/auth',
+    name: 'Auth',
+    component: ()=>import('../views/Auth/index.vue'),
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  // },
 ];
 
 const router = createRouter({
---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue" 
+import { UserOutlined,LockOutlined,UsergroupAddOutlined } from '@ant-design/icons-vue'
+export default defineComponent({
+    setup(){
+    },
+    components:{
+        UserOutlined,
+        LockOutlined,
+        UsergroupAddOutlined
+    },
+})
