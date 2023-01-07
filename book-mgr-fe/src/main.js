09840a742224530f05109034e81b95a31ba10095
shenyunhe <7520083+hehewudi@user.noreply.gitee.com>
2023-01-07 14:27:16 +0800

    完成前端图书管理基础界面

--- book-mgr-fe/src/main.js
+++ book-mgr-fe/src/main.js
@@ -4,5 +4,6 @@ import router from './router';
 import store from './store';
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css';
+import SpaceBetween from '@/components/SpaceBetween/index.vue'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+createApp(App).use(store).use(router).use(Antd).component('space-between',SpaceBetween).mount('#app');
---
+++ book-mgr-fe/src/views/Books/index.js
@@ -0,0 +1,26 @@
+import { defineComponent,reactive } from "vue";
+
+export default defineComponent({
+    setup(){
+        const columns = [
+            {
+                title:'名字',
+                dataIndex:'name'
+            },
+            {
+                title:'年龄',
+                dataIndex:'age'
+            }
+        ]
+        const dataSource = [
+            {
+                name:'小红',
+                age:2
+            }
+        ]
+        return{
+            columns,
+            dataSource
+        }
+    }
+})
